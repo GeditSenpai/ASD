1fb100744114dd7628c88050eee9dc9ecffe2ff7
GeditSenpai <dogolion.novelder@gmail.com>
2019-12-06 01:49:24 -0500

    XD3

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT= ' '
 STATIC_URL = '/statics/'
-STATICFILE_DIRS = (
-    os.path.join('statics'),
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'statics'),
 )
--- propiedades/views.py
+++ propiedades/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
 def inmobiliaria(request):
-	return render(request, 'base.html')
+	return render(request, 'index.html')
